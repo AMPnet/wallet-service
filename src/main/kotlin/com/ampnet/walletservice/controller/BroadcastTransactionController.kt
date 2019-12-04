0d3e489c25ec6af2341b3c95aa066d630f954b63
Eugen Druzin <edruzin@gmail.com>
2019-12-04 10:29:03 +0100

    Change broadcast request to json instead of url encoded
    The problem was that url encoding changed the signed transaction

--- src/main/kotlin/com/ampnet/walletservice/controller/BroadcastTransactionController.kt
+++ src/main/kotlin/com/ampnet/walletservice/controller/BroadcastTransactionController.kt
@@ -1,12 +1,13 @@
 package com.ampnet.walletservice.controller
 
+import com.ampnet.walletservice.controller.pojo.request.TxBroadcastRequest
 import com.ampnet.walletservice.controller.pojo.response.TxHashResponse
 import com.ampnet.walletservice.service.BroadcastTransactionService
 import com.ampnet.walletservice.websocket.WebSocketNotificationService
 import mu.KLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,14 +19,11 @@ class BroadcastTransactionController(
     companion object : KLogging()
 
     @PostMapping("/tx_broadcast")
-    fun broadcastTransaction(
-        @RequestParam(name = "tx_id", required = true) txId: Int,
-        @RequestParam(name = "tx_sig", required = true) signedTransaction: String
-    ): ResponseEntity<TxHashResponse> {
-        logger.info { "Received request to broadcast transaction with id: $txId" }
-        logger.debug { "Received request to broadcast transaction with sig: $signedTransaction" }
-        val txHash = broadcastService.broadcast(txId, signedTransaction)
-        notificationService.notifyTxBroadcast(txId, "BROADCAST")
+    fun broadcastTransaction(@RequestBody request: TxBroadcastRequest): ResponseEntity<TxHashResponse> {
+        logger.info { "Received request to broadcast transaction with txId: ${request.txId}" }
+        logger.debug { "Received request to broadcast transaction with txSig: ${request.txSig}" }
+        val txHash = broadcastService.broadcast(request.txId, request.txSig)
+        notificationService.notifyTxBroadcast(request.txId, "BROADCAST")
         return ResponseEntity.ok(TxHashResponse(txHash))
     }
 }
---
+++ src/main/kotlin/com/ampnet/walletservice/controller/pojo/request/TxBroadcastRequest.kt
@@ -0,0 +1,3 @@
+package com.ampnet.walletservice.controller.pojo.request
+
+data class TxBroadcastRequest(val txId: Int, val txSig: String)
