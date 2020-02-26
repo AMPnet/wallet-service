87529bf507aa5d9b18e0a85b4b8486cd27faa8f4
Eugen Druzin <edruzin@gmail.com>
2020-02-26 13:54:09 +0100

    Change investment http requests to post

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 group = "com.ampnet"
-version = "0.2.2"
+version = "0.2.3"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
--- src/main/kotlin/com/ampnet/walletservice/controller/InvestmentController.kt
+++ src/main/kotlin/com/ampnet/walletservice/controller/InvestmentController.kt
@@ -1,14 +1,15 @@
 package com.ampnet.walletservice.controller
 
+import com.ampnet.walletservice.controller.pojo.request.AmountRequest
 import com.ampnet.walletservice.controller.pojo.response.TransactionResponse
 import com.ampnet.walletservice.service.ProjectInvestmentService
 import com.ampnet.walletservice.service.pojo.ProjectInvestmentRequest
 import java.util.UUID
 import mu.KLogging
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,22 +17,19 @@ class InvestmentController(private val projectInvestmentService: ProjectInvestme
 
     companion object : KLogging()
 
-    // TODO: change both mappings to PostMapping
-
-    @GetMapping("/invest/project/{projectUuid}")
+    @PostMapping("/invest/project/{projectUuid}")
     fun generateProjectInvestmentTransaction(
         @PathVariable("projectUuid") projectUuid: UUID,
-        @RequestParam(name = "amount") amount: Long
+        @RequestBody request: AmountRequest
     ): ResponseEntity<TransactionResponse> {
         logger.debug { "Received request to generate invest transaction for project: $projectUuid" }
         val userPrincipal = ControllerUtils.getUserPrincipalFromSecurityContext()
-
-        val request = ProjectInvestmentRequest(projectUuid, userPrincipal.uuid, amount)
-        val transaction = projectInvestmentService.generateInvestInProjectTransaction(request)
+        val serviceRequest = ProjectInvestmentRequest(projectUuid, userPrincipal.uuid, request.amount)
+        val transaction = projectInvestmentService.generateInvestInProjectTransaction(serviceRequest)
         return ResponseEntity.ok(TransactionResponse(transaction))
     }
 
-    @GetMapping("/invest/project/{projectUuid}/cancel")
+    @PostMapping("/invest/project/{projectUuid}/cancel")
     fun generateCancelInvestmentsInProjectTransaction(
         @PathVariable("projectUuid") projectUuid: UUID
     ): ResponseEntity<TransactionResponse> {
