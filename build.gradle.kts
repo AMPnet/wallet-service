b999cfeee908cb90cc7c9622f67d5f0247088d93
Josip Koncurat <jkoncura@gmail.com>
2020-09-08 16:02:18 +0200

    update jwt fixes AB#442 (#48)
    * update jwt fixes AB#442
    
    * Update jwt public key
    
    Co-authored-by: Josip Končurat <josipkoncurat@Josips-MacBook-Pro.local>
    Co-authored-by: Eugen Druzin <edruzin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 group = "com.ampnet"
-version = "0.4.7"
+version = "0.5.0"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
@@ -51,7 +51,7 @@ dependencies {
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("net.devh:grpc-spring-boot-starter:2.9.0.RELEASE")
     implementation("software.amazon.awssdk:s3:2.5.27")
-    implementation("com.github.AMPnet:jwt:0.0.9")
+    implementation("com.github.AMPnet:jwt:0.1.5")
     implementation("org.iban4j:iban4j:3.2.1")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
